572f4cc2446802aa7a691626d0f7e74d5bd38372
Thiago Alexandria <thiagoo_oa@hotmail.com>
2020-01-06 20:15:16 -0300

    Add css variables

--- src/components/Search/styled.js
+++ src/components/Search/styled.js
@@ -1,38 +1,43 @@
 import styled from "styled-components"
 
 export const SearchWrapper = styled.section`
-  background: #16202c;
+  background: var(--background);
   display: flex;
   flex-direction: column;
   width: 100%;
   transition: opacity 0.4s;
+
   .ais-InstantSearch__root {
     display: flex;
     flex-direction: column;
     height: auto;
     width: 100%;
   }
+
   .ais-SearchBox,
   .ais-Stats {
     padding: 0.5rem 3rem;
   }
+
   .ais-SearchBox {
     padding-top: 6rem;
   }
+  
   .ais-Stats {
-    color: #8899a6;
+    color: var(--texts);
   }
+
   .ais-SearchBox-input {
     background: none;
     border: none;
-    border-bottom: 1px solid #38444d;
-    color: #8899a6;
+    border-bottom: 1px solid var(--borders);
+    color: var(--texts);
     display: flex;
     font-size: 1.6rem;
     padding: 0.5rem;
     width: 100%;
     &::placeholder {
-      color: #8899a6;
+      color: var(--texts);
     }
   }
   .ais-SearchBox-submit,
