a79503454aa999be5a2314ccce015dbb9facb6e2
Thiago Alexandria <thiagoo_oa@hotmail.com>
2020-01-29 09:18:18 -0300

    Remove Arrow ( Page UP )

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react"
 
 import { Home } from "styled-icons/boxicons-solid/Home"
 import { SearchAlt2 as Search } from "styled-icons/boxicons-regular/SearchAlt2"
-import { UpArrowAlt as Arrow } from "styled-icons/boxicons-regular/UpArrowAlt"
 import { Lightbulb as Light } from "styled-icons/typicons/Lightbulb"
 import { Grid } from "styled-icons/boxicons-solid/Grid"
 import { ListUl as List } from "styled-icons/boxicons-regular/ListUl"
@@ -71,9 +70,6 @@ const MenuBar = () => {
           className="display"
         >
           {isListMode ? <Grid /> : <List />}
-        </S.MenuBarItem>
-        <S.MenuBarItem title="Ir para o Topo">
-          <Arrow />
         </S.MenuBarItem>
         </S.MenuBarGroup>
     </S.MenuBarWrapper>
