950720c5dd6a338dc2d73eaa25a8b75f388a8993
Thiago Alexandria <thiagoo_oa@hotmail.com>
2020-02-12 08:32:20 -0300

    About

--- src/pages/about.js
+++ src/pages/about.js
@@ -67,13 +67,6 @@ const AboutPage = () => (
         <li>O que ainda não sei busco aprender.</li>
       </ul>
 
-      <h2>Contato</h2>
-
-      <p>
-        Você pode entrar em contato comigo através de qualquer uma das minhas
-        redes sociais.
-      </p>
-
       <SocialLinks hideStyle />
     </MainContent>
   </Layout>
