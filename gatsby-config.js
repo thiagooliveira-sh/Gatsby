a2209871205752f1c105a5967edcfc2da5d03e86
Thiago Alexandria <thiagoo_oa@hotmail.com>
2020-05-01 12:33:56 -0300

    Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,12 @@ module.exports = {
       },
     },
     {
+	resolve: `gatsby-plugin-google-analytics`,
+	options: {
+		trackingId: "UA-165278630-1",
+    		},
+    },
+    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
