4ce96323d9b83e042a200ba75554216e9d11cfb0
Thiago Alexandria <alexandriathiago@gmail.com>
2020-09-26 14:07:05 -0300

    Update content.js

--- src/components/SocialLinks/content.js
+++ src/components/SocialLinks/content.js
@@ -12,10 +12,6 @@ const links = [
     label: "Instagram",
     url: `https://www.instagram.com/thiagoalexandria/`,
   },
-  {
-    label: "Acclaim",
-    url: `https://www.youracclaim.com/users/thiago-oliveira-alexandria`,
-  },
 ]
 
 export default links
