a5ce6ac3354ecd0527c007df6ab6b67a88daef0f
Thiago Alexandria <alexandriathiago@gmail.com>
2020-09-26 13:57:22 -0300

    Update Icons.js

--- src/components/SocialLinks/Icons.js
+++ src/components/SocialLinks/Icons.js
@@ -1,11 +1,13 @@
 import { Github } from "@styled-icons/boxicons-logos/Github"
 import { LinkedinSquare } from "@styled-icons/boxicons-logos/LinkedinSquare"
 import { Instagram } from "@styled-icons/boxicons-logos/Instagram"
+import { Acclaim} from "@styled-icons/boxicons-regular/ChevronsUp"
 
 const Icons = {
   Github,
   LinkedinSquare,
   Instagram,
+  Acclaim,
 }
 
 export default Icons
