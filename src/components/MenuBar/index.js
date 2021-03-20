a43593818f6bc72d4f5e5ee2d5b2f441e3810c6c
thiago.alexandria <thiago.alexandria@conductor.com.br>
2021-03-20 14:58:10 -0300

    add about in menu bar

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react"
 import { Home } from "@styled-icons/boxicons-solid/Home"
 import { SearchAlt2 as Search } from "@styled-icons/boxicons-regular/SearchAlt2"
 import {Dollar} from "@styled-icons/boxicons-regular/Dollar"
+import {PersonFill} from "@styled-icons/bootstrap/PersonFill"
 import { Lightbulb as Light } from "@styled-icons/typicons/Lightbulb"
 import { Grid } from "@styled-icons/boxicons-solid/Grid"
 import { ListUl as List } from "@styled-icons/boxicons-regular/ListUl"
@@ -38,6 +39,18 @@ const MenuBar = () => {
           >
           <S.MenuBarItem>
             <Home />
+          </S.MenuBarItem>
+        </S.MenuBarLink>
+        <S.MenuBarLink
+            to="/about/"
+            cover
+            direction="right"
+            bg={getThemeColor()}
+            duration={0.6}
+            title="Doação"
+          >
+          <S.MenuBarItem>
+            <PersonFill />
           </S.MenuBarItem>
         </S.MenuBarLink>
         <S.MenuBarLink
